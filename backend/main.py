25507b8f0dbab93888e57ba32f8a4bdaaed04331
Sunn83 <taxpress2013@hotmail.gr>
2025-10-06 14:31:12 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,20 @@ import os
 from fastapi import FastAPI, Query
 from fastapi.staticfiles import StaticFiles
 from index_docs import index_all_documents, search_documents
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="AI Docs App")
+# Ενεργοποίηση CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ή ["http://144.91.115.48:3000"] για πιο ασφαλή επιλογή
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
 
 # Δρομολογούμε το frontend μόνο αν υπάρχει
 frontend_path = os.path.join(os.path.dirname(__file__), "../frontend")
