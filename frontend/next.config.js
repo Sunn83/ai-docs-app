bb8f412f9a945cf72cd62b464f90893fb9285be2
Sunn83 <taxpress2013@hotmail.gr>
2025-10-06 14:19:03 +0300

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://144.91.115.48:8000/api/:path*',
+      },
+    ];
   },
 };
-
-module.exports = nextConfig;
