7022cb97d0e481291c392ccdafc4fae74178cf9c
Sunn83 <taxpress2013@hotmail.gr>
2025-10-06 14:22:22 +0300

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,13 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://144.91.115.48:8000/api/:path*',
+        destination: 'http://ai-docs-app_backend_1:8000/api/:path*', // docker container name
       },
     ];
   },
 };
+
+module.exports = nextConfig;
