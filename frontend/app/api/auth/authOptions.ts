abbeb7d82e3de250babe7d24be24c558278a68eb
Sunn83 <taxpress2013@hotmail.gr>
2025-10-09 10:40:27 +0300

    Create authOptions.ts

---
+++ frontend/app/api/auth/authOptions.ts
@@ -0,0 +1,31 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const username = process.env.NEXTAUTH_USERNAME;
+        const password = process.env.NEXTAUTH_PASSWORD;
+
+        if (
+          credentials?.username === username &&
+          credentials?.password === password
+        ) {
+          return { id: "1", name: "Admin" };
+        }
+
+        return null;
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/login",
+  },
+};
