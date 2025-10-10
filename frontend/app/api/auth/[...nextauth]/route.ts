aabb3d6d0783fe641bfbd239b6bb6f80835fecc6
Sunn83 <taxpress2013@hotmail.gr>
2025-10-10 13:23:29 +0300

    Update route.ts

--- frontend/app/api/auth/[...nextauth]/route.ts
+++ frontend/app/api/auth/[...nextauth]/route.ts
@@ -1,36 +1,6 @@
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
+import { authOptions } from "../auth/authOptions";
 
-const handler = NextAuth({
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        username: { label: "Όνομα χρήστη", type: "text" },
-        password: { label: "Κωδικός", type: "password" },
-      },
-      async authorize(credentials) {
-        const username = process.env.NEXTAUTH_USERNAME;
-        const password = process.env.NEXTAUTH_PASSWORD;
-
-        if (
-          credentials?.username === username &&
-          credentials?.password === password
-        ) {
-          return { id: "1", name: "Admin" };
-        }
-        return null;
-      },
-    }),
-  ],
-  pages: {
-    signIn: "/login",
-    error: "/login/error",
-  },
-  session: {
-    strategy: "jwt",
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-});
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
